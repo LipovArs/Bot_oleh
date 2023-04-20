f5e6f72ba3b2ac59e63d0beb9558c674343ef07c
DimRodgers <mail.gmail.com>
2023-04-20 12:46:46 +0300

    Gaussian

--- src/bot_func.py
+++
@@ -1,9 +0,0 @@
-CONTENT_WORDS = ["жид", "жиди", "жидо-бандерівець", "жидо-бандеравець", "жидобор", "жидо-бандера", "жидобандерівець", "jid", "жидів", "жидами", "жида", "жидах", "жидом", "жиду", "жидам", "жиді", ";bl", ":bl", ";blb", ":blb"]
-
-def help_fun():
-    """"Help function"""
-    return 'Here is some help about bot'
-
-
-def get_ban_words():
-    return CONTENT_WORDS
