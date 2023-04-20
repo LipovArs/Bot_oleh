25961365ee190ff825a026dc3c8b43763b80e2d5
Arseniy <lypovyi2020ks15@student.karazin.ua>
2023-04-20 13:46:48 +0300

    +

--- src/bot.py
+++ src/bot.py
@@ -1,22 +1,27 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
+from discord import Game
 import os
 import bot_func
 
 intents = discord.Intents.all()
 intents.message_content = True
+intents.members = True
+intents.presences = True
 
 config = {
     'prefix': '.'
 }
 
 bot = commands.Bot(command_prefix=config['prefix'], intents=intents)
+client = discord.Client(intents=intents)
 
 
 @bot.event
 async def on_ready():
     print(f"Bot {bot.user} is ready")
+    await bot.change_presence(activity=Game(".help"))
 
 
 @bot.event
@@ -42,6 +47,15 @@ async def on_message(message):
                 if content == course_word:
                     await message.delete()
                     await message.channel.send(f"{message.author.mention} ти не маєш ліцензії на слово ЖИД")
+
+
+@client.event
+async def on_member_update(before, after):
+    if before.activities != after.activities:
+        if len(after.activities) == 0:
+            print(f"{after.name} has stopped playing a game.")
+        elif str(after.activities[0].type) == "ActivityType.playing":
+            print(f"{after.name} is now playing {after.activities[0].name}.")
 
 
 bot.run(os.environ['BOT_DS_TOKEN'])
