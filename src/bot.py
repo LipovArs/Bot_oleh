011f9c3c5cf73bdf221f9a98e337268c3daba45c
Arseniy <lypovyi2020ks15@student.karazin.ua>
2023-05-01 23:15:17 +0300

    Commit of new version of project

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,8 @@ from discord.utils import get
 from discord import Game
 import os
 import bot_func
+import datetime
+import youtube_dl
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,13 +19,13 @@ config = {
 game_stats = {}
 
 bot = commands.Bot(command_prefix=config['prefix'], intents=intents)
+client = discord.Client(intents=intents)
 
 
 @bot.event
 async def on_ready():
     print(f"Bot {bot.user} is ready")
     await bot.change_presence(activity=Game(".help"))
-
 
 @bot.event
 async def on_message(message):
@@ -37,39 +39,35 @@ async def on_message(message):
         await message.channel.send(bot_func.gaussian_method(message.content))
 
     elif message.content.startswith('.stats'):
-        members = message.guild.members
-        activities = {}
-        for member in members:
-            for activity in member.activities:
-                if activity.type == discord.ActivityType.playing:  # перевіряємо, що активність є грою
-                    game_name = activity.name
-                    member_nick = member.nick or member.name
-                    activities[f"{member_nick} - {game_name}"] = ''
+        await message.channel.send(bot_func.stat_msg(message))
 
-        output = "Activity Stats:\n"
-        formatted_activities = "\n".join(f"{key} {value}" for key, value in activities.items())
+    elif message.content.startswith('.inf'):
+        await bot_func.user_info(message)
 
-        await message.channel.send(output + formatted_activities)
-
+    if message.content.startswith('.play'):
+        if message.author.voice:
+            channel = message.author.voice.channel
+            await channel.connect()
+        else:
+            await message.channel.send("Kakish")
+    
     elif message.content.startswith('.role'):
-        roles = [role.name for role in message.author.roles]
+        roles = message.author.roles
         await message.channel.send(roles)
 
     elif message.content.startswith('.'):
         await message.channel.send("Unknown command.")
 
-    roles_list = [role.name for role in message.author.roles]
-    message_test = bot_func.message_control(message.content, roles_list)
-    if not message_test:
-        pass
-    elif message_test == 'Jid_detected':
-        await message.delete()
-        await message.channel.send(f"{message.author.mention} ти не маєш ліцензії на слово ЖИД")
-    else:
-        await message.delete()
-        await message.channel.send(f"{message.author.mention}, {message_test}")
-
-
+    # roles_list = [role.name for role in message.author.roles]
+    # message_test = bot_func.message_view(message.content.lower(), roles_list)
+    # if not message_test:
+    #     pass
+    # elif message_test == 'Jid_detected':
+    #     await message.delete()
+    #     await message.channel.send(f"{message.author.mention} ти не маєш ліцензії на слово ЖИД")
+    # else:
+    #     await message.delete()
+    #     await message.channel.send(f"{message.author.mention}, {message_test}")
 
 
 bot.run(os.environ['BOT_DS_TOKEN'])
--- src/bot_func.py
+++ src/bot_func.py
@@ -1,5 +1,9 @@
 import gaussian_sollution
 import message_control
+import discord
+from discord import Game
+import bot_func
+
 CONTENT_WORDS = ["жид", "жиди", "жидо-бандерівець", "жидо-бандеравець", "жидобор", "жидо-бандера", "жидобандерівець",
                  "jid", "жидів", "жидами", "жида", "жидах", "жидом", "жиду", "жидам", "жиді", ";bl", ":bl", ";blb",
                  ":blb"]
@@ -23,5 +27,68 @@ def gaussian_method(msg):
     return gaussian_sollution.final_res(formula)
 
 
-def message_control(msg, roles):
+def message_view(msg, roles):
     return message_control.check(msg, roles)
+
+
+def stat_msg(message):
+    members = message.guild.members
+    activities = {}
+    for member in members:
+        for activity in member.activities:
+            if activity.type == discord.ActivityType.playing:
+                game_name = activity.name
+                member_nick = member.nick or member.name
+                activities[f"{member_nick} - {game_name}"] = ''
+            elif activity.type == discord.ActivityType.listening:
+                listen_name = activity.name
+                member_nick = member.nick or member.name
+                activities[f"{member_nick} - {listen_name}"] = ''
+
+    output = "Activity Stats:\n"
+    formatted_activities = "\n".join(f"{key} {value}" for key, value in activities.items())
+
+    res = output + formatted_activities
+
+    return res
+
+
+async def user_info(message):
+    moderator_role = discord.utils.get(message.guild.roles, name='Moderator')
+    if message.author.guild_permissions.administrator or moderator_role in message.author.roles:
+        user_input = message.content
+        parts = user_input.split()
+        if parts[0] == '.inf':
+            user_input_processed = ' '.join(parts[:1] + [part.replace(' ', '_') for part in parts[1:]])
+            user_input_processed = user_input_processed.replace('.inf_', '.inf ')
+            parts_processed = user_input_processed.split()
+            user_enter_nick = " ".join(parts_processed[1:])
+            if len(parts_processed) < 2:
+                await message.channel.send('Недостатньо аргументів.')
+            else:
+                guild = message.guild
+                users_found = []
+                for member in guild.members:
+                    if member.nick == user_enter_nick or member.name == user_enter_nick:
+                        users_found.append(member)
+                if users_found:
+                    for user in users_found:
+                        user_name = user.nick or user.name
+                        if not user_name:
+                            user_name = user.name or user.nick
+                        user_role = str([role.name for role in user.roles if role.id != 1026612675830099988])
+                        joined_at = user.joined_at
+                        user_avatar_url = None
+                        created_at = user.created_at
+                        try:
+                            if not user.avatar.url == 'NoneType':
+                                user_avatar_url = user.avatar.url
+                        except:
+                            print("None Avatar")
+                        await message.channel.send(
+                            f"User - {user}\nNick - {user_name}\nRoles - {user_role}\nJoined at - {joined_at}\n" +
+                            f"Created at - {created_at}\nAvatar URL - {user_avatar_url}")
+                else:
+                    await message.channel.send('Nope')
+    else:
+        await message.channel.send("В вас недостатньо прав.")
